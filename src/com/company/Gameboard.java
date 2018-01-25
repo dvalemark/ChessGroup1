168295d0fa974ba7552f9f38e03eaa0c13af07f6
Robin Larsson <Robinhiio@hotmail.com>
2018-01-25 16:30:35 +0100

    Replaced letters with backgroundcolor on visual gameboard

--- src/com/company/Gameboard.java
+++ src/com/company/Gameboard.java
@@ -1,52 +1,50 @@
 package com.company;
 
 import com.googlecode.lanterna.terminal.Terminal;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Gameboard {
-    private Map<Position,Piece> gameboard;
+    private Map<Position, Piece> gameboard;
+    final int WIDTH = 8;
 
-    public Gameboard(){
+    public Gameboard() {
         this.gameboard = new HashMap<>();
         addSquares();
     }
 
-    private void addSquares(){
+    private void addSquares() {
         Color color;
-        for (int y = 0; y < 8; y++) {
-        for (int x = 0; x < 8 ; x++) {
-                if(y%2==0){
-                    if(x%2!=0) {
+        for (int y = 0; y < WIDTH; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+                if (y % 2 == 0) {
+                    if (x % 2 != 0) {
                         color = Color.WHITE;
-                    }else {
+                    } else {
                         color = Color.BLACK;
                     }
-                }else{
-                    if(x%2 == 0){
+                } else {
+                    if (x % 2 == 0) {
                         color = Color.WHITE;
-                    }else {
+                    } else {
                         color = Color.BLACK;
                     }
                 }
-            gameboard.put(new Position(x,y,color),null);
-                }
+                gameboard.put(new Position(x, y, color), null);
+            }
         }
     }
 
-    public void drawSquares(com.googlecode.lanterna.screen.Screen screen){
-        String color;
+    public void drawSquares(com.googlecode.lanterna.screen.Screen screen) {
         for (Map.Entry<Position, Piece> entry : gameboard.entrySet()) {
-            if(entry.getKey().color == Color.WHITE){
-                 color = "W";
-            }else {
-                color = "B";
+            if (entry.getKey().color == Color.WHITE) {
+                screen.putString(entry.getKey().x, entry.getKey().y, " ", Terminal.Color.BLACK, Terminal.Color.WHITE);
+            } else {
+                screen.putString(entry.getKey().x, entry.getKey().y, " ", Terminal.Color.BLACK, Terminal.Color.BLACK);
             }
-            screen.putString(entry.getKey().x, entry.getKey().y, color, Terminal.Color.WHITE, Terminal.Color.BLACK);
-            System.out.println("X:" + entry.getKey().x + " Y:" + entry.getKey().y + " Color:" + entry.getKey().color);
         }
         screen.refresh();
-        System.out.println(gameboard.entrySet());
     }
 
 }
