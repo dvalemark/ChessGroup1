afec6aaa65726957e0e08073d0108daf233dfafa
Robin Larsson <Robinhiio@hotmail.com>
2018-02-01 18:58:10 +0100

    implement move logic for pawn

---
+++ src/com/company/King.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class King extends Piece {
+
+    public King(Color color) {
+        super(color);
+        this.kind = Kind.KING;
+    }
+}
---
+++ src/com/company/Pawn.java
@@ -0,0 +1,59 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Pawn extends Piece {
+    private boolean isFirstMove;
+
+    public Pawn(Color color) {
+        super(color);
+        this.kind = Kind.PAWN;
+        this.isFirstMove = true;
+        this.value = 1;
+    }
+
+    @Override
+    public ArrayList<Move> checkMoves(int y, int x) {
+        MoveHelper moveHelper = new MoveHelper();
+        ArrayList<Move> moves = new ArrayList<>();
+        int direction;
+        int value = 0;
+        if (this.getColor() == Color.WHITE) {
+            direction = -1;
+        } else {
+            direction = 1;
+        }
+        ////IF PAWN HASN'T MOVE BEFORE
+        if (isFirstMove) {
+            int i = 1;
+            while (moveHelper.isForward(y, x, i, direction) == null && i != 3) {
+                moves.add(new Move(value, y, (y - i), x, x));
+                i++;
+            }
+            ////NORMAL FORWARD MOVE
+        } else {
+            for (int i = 1; i <= 1; i++) {
+                if (moveHelper.isForward(y, x, i, direction) == null) {
+                    moves.add(new Move(value, y, (y - i), x, x));
+                }
+            }
+        }
+        //////THESE ARE THE POSSIBLE ATTACK MOVES
+        Piece possibleEnemy = moveHelper.isForwardLeft(y, x, 1, direction);
+        if (possibleEnemy != null && possibleEnemy.getColor() != this.getColor()) {
+            value = possibleEnemy.getValue();
+            moves.add(new Move(value, y, y + direction, x, x - 1));
+            value = 0;
+        }
+        possibleEnemy = moveHelper.isForwardRight(y, x, 1, direction);
+        if (possibleEnemy != null && possibleEnemy.getColor() != this.getColor()) {
+            value = possibleEnemy.getValue();
+            moves.add(new Move(value, y, y + direction, x, x + 1));
+            value = 0;
+        }
+        ////PRINT ALL POSSIBLE MOVES FOR THIS PIECE
+        moves.stream().forEach(System.out::println);
+
+        return moves;
+    }
+}
