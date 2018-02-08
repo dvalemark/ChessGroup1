62e886e20e3d325ce3b6f88df4ec0120bf629a17
Robin Larsson <Robinhiio@hotmail.com>
2018-02-08 12:47:12 +0100

    typo error

--- src/com/company/Rook.java
+++ src/com/company/Rook.java
@@ -1,8 +1,8 @@
 package com.company;
 
-public class Rock extends Piece {
+public class Rook extends Piece {
 
-    public Rock(Color color) {
+    public Rook(Color color) {
         super(color);
         this.kind = Kind.ROCK;
         this.value = 1;
