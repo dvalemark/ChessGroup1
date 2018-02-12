5ac35a51bc400ecc0e8617b953f392be5fae7c86
dvalemark <32665808+dvalemark@users.noreply.github.com>
2018-02-12 14:28:31 +0100

    Update MoveHelper.java

--- src/com/company/MoveHelper.java
+++ src/com/company/MoveHelper.java
@@ -57,7 +57,7 @@ public class MoveHelper {
         Piece pieceToMove = tile[move.fromY][move.fromX].getPiece();
         tile[move.fromY][move.fromX].setPiece(null);
         tile[move.toY][move.toX].setPiece(pieceToMove);
-      //  System.out.println("MOVED " + pieceToMove.toString() + " TO " + move.toY + ":" + move.toX);
+      
 
 
         pieceToMove.firstMove = false;
