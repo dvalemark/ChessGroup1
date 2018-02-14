cedebf14c0bdf1a193cf42af84b9bebe6db0d454
Robin Larsson <Robinhiio@hotmail.com>
2018-02-14 14:23:45 +0100

    fixed move bug

--- src/com/company/MoveHelper.java
+++ src/com/company/MoveHelper.java
@@ -35,23 +35,7 @@ public class MoveHelper {
         return tile[y][x+ range].getPiece();
     }
 
-    public void analyzeMoves(Color color) {
-        ArrayList<Move> moves = new ArrayList<>();
-        for (int y = 0; y < WIDTH; y++) {
-            for (int x = 0; x <WIDTH; x++) {
-                if (tile[y][x].getPiece() != null && tile[y][x].getPiece().getColor() == color) {
-                    Piece piece = tile[y][x].getPiece();
-                    moves.addAll(piece.checkMoves(y, x));
-                }
-            }
-        }
-        Collections.shuffle(moves);
-        Move bestMove = moves
-                .stream()
-                .max(Comparator.comparing(Move::getValue)).orElseThrow(NoSuchElementException::new);
-        movePiece(bestMove);
-    }
-//////BISHOP METHODS///////////////
+    //////BISHOP METHODS///////////////
     public Piece upLeft(int y, int x, int range) {
         return tile[y - range][x - range].getPiece();
     }
@@ -64,7 +48,29 @@ public class MoveHelper {
     public Piece downLeft(int y, int x, int range) {
         return tile[y + range][x - range].getPiece();
     }
-  ///////////////////////////////////////
+    ///////////////////////////////////////
+
+    public void analyzeMoves(Color color) {
+        ArrayList<Move> moves = new ArrayList<>();
+        for (int y = 0; y < WIDTH; y++) {
+            for (int x = 0; x <WIDTH; x++) {
+                if (tile[y][x].getPiece() != null && tile[y][x].getPiece().getColor() == color) {
+                    Piece piece = tile[y][x].getPiece();
+                    moves.addAll(piece.checkMoves(y, x));
+                }
+            }
+        }
+        Collections.shuffle(moves);
+        if(moves.size() > 1) {
+            Move bestMove = moves
+                    .stream()
+                    .max(Comparator.comparing(Move::getValue)).orElseThrow(NoSuchElementException::new);
+            movePiece(bestMove);
+        }else {
+            movePiece(moves.get(0));
+        }
+    }
+
 
 
     public void movePiece(Move move) {
